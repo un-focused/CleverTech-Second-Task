67aefac578960bedc019da71632cc0cf442cd381
Un Focused <unfocused@Uns-iMac-3.local>
2023-01-19 14:52:23 -0800

    basic non async code written that passes some tests

--- index.js
+++ index.js
@@ -1,8 +1,72 @@
-const pooledDownload = (connect, save, downloadList, maxConcurrency) => {
-  return connect().then((connection) => {
-    const { download, close } = connection
-    return download(downloadList[0]).then((result) => save(result)) // download the first file and save the result
-  })
+// connection opens a connection to a server
+// save saves the contents of a file
+// save(fileContents)
+// array of strings download List contains all urls of files
+// maxConcurrency, how large a pool is to be
+
+
+function sleep(ms) {
+  return new Promise(
+      (resolve) => {
+        setTimeout(
+            () => resolve(),
+            ms
+        )
+      }
+  )
 }
+
+const pooledDownload = async (connect, save, downloadList, maxConcurrency) => {
+  // open as many connections as max concurrency or download list length
+  const poolSize = Math.min(downloadList.length, maxConcurrency);
+  const toBeDownloadedQueue = downloadList.slice();
+  const cache = {};
+  let totalConnections = 0;
+
+  if (totalConnections >= maxConcurrency) {
+    throw new Error('connection failed');
+  }
+
+  async function downloadFile(connect, save, url, connections) {
+    const connection = await connect();
+    // open a connection
+    const { download, close } = connection;
+
+    try {
+      const result = await download(url);
+      await save(result);
+
+      // close is synchronous
+      close();
+    } catch(error) {
+      close();
+      throw error;
+    }
+  }
+
+  // console.log('toBeDownloadedQueue.length', toBeDownloadedQueue.length);
+  // console.log('counter', totalConnections, 'poolSize', poolSize);
+  while (toBeDownloadedQueue.length !== 0) {
+    // console.log('toBeDownloadedQueue.length', toBeDownloadedQueue.length);
+
+    // console.log('counter', totalConnections, 'poolSize', poolSize);
+    // console.log('totalConnections < poolSize', totalConnections < poolSize);
+
+    // open x connections
+    if (totalConnections < poolSize) {
+      // get url from the queue
+      const url = toBeDownloadedQueue.pop();
+
+      console.log('downloading url', url);
+      ++totalConnections;
+
+      await downloadFile(connect, save, url);
+
+      --totalConnections;
+    }
+  }
+};
+
+// void pooledDownload()
 
 module.exports = pooledDownload
